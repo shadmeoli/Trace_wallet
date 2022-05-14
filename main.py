1713ed2e54e496c5c274b72641e16757fab6cf16
shadmeoli <shadcodes@gmail.com>
2022-05-15 00:57:27 +0300

    v.0.2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # cardano wallet app UI
 
+from msilib.schema import Error
 import tkinter
 from tkinter import messagebox, scrolledtext, Toplevel
 from tkinter import ttk
@@ -15,12 +16,15 @@ import csv
 import psycopg2
 from PIL import Image, ImageTk
 
+# creating the wallate with a payment key and payment address
+from pycardano import Address, Network, PaymentSigningKey, PaymentVerificationKey
+
 # UI theming configuration
-ctk.set_appearance_mode("gray40")
+ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
 
-# KYC
+# KYC UI
 class Auth:
 
     # windows dimentions
@@ -36,14 +40,6 @@ class Auth:
     # y-axis padding
     Y_PADDING= 13
 
-    # placing the app's logo in the welcome screen
-    logo = Image.open("C:/Users/shad/Desktop/wallet/Images/wallet-icon-3.png")
-    logo_image = ImageTk.PhotoImage(logo)
-
-    welcome_logo = tkinter.Label(image=logo_image)
-    welcome_logo = logo_image
-
-
     def __init__(self):
         
         # user signup / sign in authentication andverification
@@ -54,13 +50,26 @@ class Auth:
     
     # home
     def home(self):
-        print("home page")
+        print("Home")
+    
+    # users verification (new and existing users)
+    def AllUserVerification(self, username, password):
+
+        # send an otp to the user for verification to thier mail (SMTP)
+        pass
 
     
     # registering the new user
     def save_new_user(self):
         
-        print("registered new user")
+        # getting the users input
+        self.NEW_USER = self.new_username.get()
+        self.NEW_USER_PASSWORD = self.new_password.get()
+
+        # generating a response
+        print(self.NEW_USER, ' ', self.NEW_USER_PASSWORD)
+        
+
 
     # sing up pop
     def sign_up(self):
@@ -71,31 +80,54 @@ class Auth:
 
         # welcome text
         self.welcome = ctk.CTkLabel(self.signUp_window, text="Welcome to Trace")
-        self.welcome.place(x=120, y=20)
+        self.welcome.place(x=120, y=10)
+
+        # create label on CTkToplevel window
+        self.fullName = ctk.CTkLabel(self.signUp_window, text="Full Name")
+        self.fullName.place(x=50, y=40)
+
+        # username entry
+        self.new_emailAddress = ctk.CTkEntry(self.signUp_window)
+        self.new_emailAddress.place(x=150, y=40)
+
+        # create label on CTkToplevel window
+        self.email = ctk.CTkLabel(self.signUp_window, text="Email")
+        self.email.place(x=50, y=100)
+
+        # username entry
+        self.new_emailAddress = ctk.CTkEntry(self.signUp_window)
+        self.new_emailAddress.place(x=150, y=100)
 
         # create label on CTkToplevel window
         self.username = ctk.CTkLabel(self.signUp_window, text="username")
-        self.username.place(x=50, y=80)
+        self.username.place(x=50, y=160)
 
         # username entry
         self.new_username = ctk.CTkEntry(self.signUp_window)
-        self.new_username.place(x=150, y=80)
+        self.new_username.place(x=150, y=160)
 
         # create label on CTkToplevel window
         self.password = ctk.CTkLabel(self.signUp_window, text="password")
-        self.password.place(x=50, y=140)
+        self.password.place(x=50, y=220)
 
         # password entry
         self.new_password = ctk.CTkEntry(self.signUp_window, show='*')
-        self.new_password.place(x=150, y=140)
+        self.new_password.place(x=150, y=220)
 
         # sign up the new user
         self.signup = ctk.CTkButton(master=self.signUp_window, text="SIGN UP", command=self.save_new_user)
-        self.signup.place(x=140, y=200)
+        self.signup.place(x=140, y=280)
+
+        self.RegisteredUser = ctk.CTkLabel(self.signUp_window, text="Already registered ?")
+        self.RegisteredUser.place(x=50, y=350)
+
+        # sign up the new user
+        self.RegisteredUserSignin = ctk.CTkButton(master=self.signUp_window, text=" sign in ", width=8, height=3, border_width=0, background="gray20", command=self.sign_in)
+        self.RegisteredUserSignin.place(x=200, y=350)
     
     # sign in pop
     def sign_in(self):
-
+        
         # top levelp for the sign up
         self.signIn_window = ctk.CTkToplevel(self.win)
         self.signIn_window.geometry("400x400")
@@ -106,8 +138,8 @@ class Auth:
         self.welcome.place(x=120, y=20)
 
         # create label on CTkToplevel window
-        self.username = ctk.CTkLabel(self.signIn_window, text="username")
-        self.username.place(x=50, y=80)
+        self.username = ctk.CTkLabel(self.signIn_window, text="username/email")
+        self.username.place(x=35, y=80)
 
         # username entry
         self._username = ctk.CTkEntry(self.signIn_window)
